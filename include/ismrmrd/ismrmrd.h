d0d9d6df9c928f606ce19fbe6d59d1847ee5ce8b
Martyn Klassen <lmklassen@gmail.com>
2020-05-25 18:18:42 -0400

    Added header constructors from base type objects

--- include/ismrmrd/ismrmrd.h
+++ include/ismrmrd/ismrmrd.h
@@ -487,6 +487,7 @@ public:
     // Constructors
     AcquisitionHeader();
 
+    AcquisitionHeader(ISMRMRD_AcquisitionHeader const &header): ISMRMRD_AcquisitionHeader(header) {};
 
     bool operator==(const AcquisitionHeader& acq) const;
 
@@ -632,6 +633,8 @@ class EXPORTISMRMRD ImageHeader: public ISMRMRD_ImageHeader {
 public:
     // Constructor
     ImageHeader();
+
+    ImageHeader(ISMRMRD_ImageHeader const &header): ISMRMRD_ImageHeader(header) {};
 
     // Flag methods
     bool isFlagSet(const uint64_t val);
